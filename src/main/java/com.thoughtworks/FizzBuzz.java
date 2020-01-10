092548bb867943c5e49fc33b94715b09ab618193
qinghua <qinghua.liu@thoughtworks.com>
2020-01-10 10:46:30 +0800

    [Qinghua Liu] feat: get combination when the digital is common multiple of 3 5 7.

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -4,13 +4,20 @@ public class FizzBuzz {
 
     public String game(int target) {
         Digital digital = new Digital();
-        if (digital.is3Multiples(target)) {
-            return "Fizz";
-        } else if (digital.is5Multiples(target)) {
-            return "Buzz";
-        } else if (digital.is7Multiples(target)) {
-            return "Whizz";
+        StringBuffer result = new StringBuffer();
+        if (digital.is3Or5Or7Multiples(target)) {
+            if (digital.is3Multiples(target)) {
+                result.append("Fizz");
+            }
+            if (digital.is5Multiples(target)) {
+                result.append("Buzz");
+            }
+            if (digital.is7Multiples(target)) {
+                result.append("Whizz");
+            }
+        } else {
+            result.append(target);
         }
-        return Integer.toString(target);
+        return result.toString();
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -16,24 +16,29 @@ public class FizzBuzzTest {
 
     @Test
     public void should_return_fizz_when_digital_is_multiples_of_3() {
-        String actual = fizzBuzz.game(3);
+        String actual = fizzBuzz.game(9);
         String excepted = "Fizz";
         assertEquals(excepted, actual);
     }
 
     @Test
     public void should_return_buzz_when_digital_is_multiples_of_5() {
-        String actual = fizzBuzz.game(5);
+        String actual = fizzBuzz.game(10);
         String excepted = "Buzz";
         assertEquals(excepted, actual);
     }
 
     @Test
     public void should_return_whizz_when_digital_is_multiples_of_7() {
-        String actual = fizzBuzz.game(7);
+        String actual = fizzBuzz.game(14);
         String excepted = "Whizz";
         assertEquals(excepted, actual);
     }
 
-
+    @Test
+    public void should_return_combination_when_digital_is_multiples_of_3_5_7() {
+        String actual = fizzBuzz.game(21);
+        String excepted = "FizzWhizz";
+        assertEquals(excepted, actual);
+    }
 }
