8121cc8ee61c30fc5f8ee7a736105b25036df263
qinghua <qinghua.liu@thoughtworks.com>
2020-01-10 16:17:21 +0800

    [Qinghua Liu] fix the null bug and contains 5&3 bug.

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,6 +2,15 @@ package com.thoughtworks;
 
 public class FizzBuzz {
 
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 1; i < 121; i++) {
+            String result = fizzBuzz.game(i);
+            System.out.println(result);
+        }
+    }
+
+
     public String game(int target) {
         Digital digital = new Digital();
         StringBuilder result = new StringBuilder();
@@ -12,11 +21,13 @@ public class FizzBuzz {
                 } else {
                     getResult(result, digital.is3Multiples(target), "Fizz");
                     getResult(result, digital.is7Multiples(target), "Whizz");
+                    getResult(result, result.length() == 0, Integer.toString(target));
                 }
             } else {
                 if (digital.isContains5(target)) {
                     getResult(result, digital.is5Multiples(target), "Buzz");
                     getResult(result, digital.is7Multiples(target), "Whizz");
+                    getResult(result, result.length() == 0, Integer.toString(target));
                 } else if (digital.isContains3(target)) {
                     getResult(result, digital.is3Multiples(target), "Fizz");
                 } else {
@@ -26,10 +37,14 @@ public class FizzBuzz {
                 }
             }
         } else {
-            if (digital.isContains3(target)) {
-                result.append("Fizz");
-            } else {
+            if (digital.isContains5(target)) {
                 result.append(target);
+            } else {
+                if (digital.isContains3(target)) {
+                    result.append("Fizz");
+                } else {
+                    result.append(target);
+                }
             }
         }
         return result.toString();
