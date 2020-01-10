8e037b696497893b09e553e3286c36e01d0edc4c
qinghua <qinghua.liu@thoughtworks.com>
2020-01-10 14:34:06 +0800

    [Qinghua Liu] feat: don't get buzz when digital contains 7.

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -6,15 +6,24 @@ public class FizzBuzz {
         Digital digital = new Digital();
         StringBuilder result = new StringBuilder();
         if (digital.is3Or5Or7Multiples(target)) {
-            if (digital.isContains5(target)) {
-                if (digital.is5Multiples(target)) result.append("Buzz");
-                if (digital.is7Multiples(target)) result.append("Whizz");
-            } else if (digital.isContains3(target)) {
-                if (digital.is3Multiples(target)) result.append("Fizz");
+            if (digital.isContains7(target)) {
+                if (digital.isContains3(target)) {
+                    if (digital.is3Multiples(target)) result.append("Fizz");
+                } else {
+                    if (digital.is3Multiples(target)) result.append("Fizz");
+                    if (digital.is7Multiples(target)) result.append("Whizz");
+                }
             } else {
-                if (digital.is3Multiples(target)) result.append("Fizz");
-                if (digital.is5Multiples(target)) result.append("Buzz");
-                if (digital.is7Multiples(target)) result.append("Whizz");
+                if (digital.isContains5(target)) {
+                    if (digital.is5Multiples(target)) result.append("Buzz");
+                    if (digital.is7Multiples(target)) result.append("Whizz");
+                } else if (digital.isContains3(target)) {
+                    if (digital.is3Multiples(target)) result.append("Fizz");
+                } else {
+                    if (digital.is3Multiples(target)) result.append("Fizz");
+                    if (digital.is5Multiples(target)) result.append("Buzz");
+                    if (digital.is7Multiples(target)) result.append("Whizz");
+                }
             }
         } else {
             if (digital.isContains3(target)) {
