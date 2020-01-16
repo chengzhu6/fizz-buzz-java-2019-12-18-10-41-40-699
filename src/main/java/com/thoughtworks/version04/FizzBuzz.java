c6846596af567ca8ffacaa58f8b30009291caa24
qinghua <qinghua.liu@thoughtworks.com>
2020-01-16 14:38:04 +0800

    [Qinghua Liu]feat: get fizz when digital is multiples of 3.

---
+++ src/main/java/com/thoughtworks/version04/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.version04;
+
+public class FizzBuzz {
+    public String fizzBuzz(Integer digital) {
+        return digital % 3 == 0 ? "Fizz" : digital.toString();
+    }
+}
